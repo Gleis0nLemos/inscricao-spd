07fce8c52192ddea3d16f2d79f6533d87bd9ef0e
Gleison Lemos <100967809+Gleis0nLemos@users.noreply.github.com>
2022-10-21 18:49:11 +0000

    chore: subscript save

--- app/controllers/subscripts_controller.rb
+++ app/controllers/subscripts_controller.rb
@@ -2,4 +2,16 @@ class SubscriptsController < ApplicationController
   def new
     @subscript = Subscript.new
   end
+  
+  def create
+    @subscript = Subscript.new(subscript_params)
+    unless @subscript.save
+      redirect_to new_subscript_path
+    end
+  end
+
+  private
+  def subscript_params
+    params.require(:subscript).permit(:name, :email, :phone, :description, :language, :schooling)
+  end
 end
